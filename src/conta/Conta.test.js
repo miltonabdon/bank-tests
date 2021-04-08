db9b90fbeab644d02562200965ccb8e3ed246209
Milton <miltonabdon69@gmail.com>
2021-04-07 22:10:41 -0300

    teste de  evento de click

--- src/conta/Conta.test.js
+++ src/conta/Conta.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import React from 'react';
 import Conta from './Conta'
 
@@ -9,5 +9,16 @@ describe('Componente de conta', () => {
         const saldo = screen.getByTestId('saldo-conta')
 
         expect(saldo.textContent).toBe('R$ 1000')
+    })
+    
+    it('Chama a função de realizar transação, quando o botão é clicado', () => {
+
+        const funcaoRealizarTransação = jest.fn()
+
+        render(<Conta saldo={1000} realizarTransacao={funcaoRealizarTransação}/>)
+
+        fireEvent.click(screen.getByText('Realizar operação'))
+
+        expect(funcaoRealizarTransação).toHaveBeenCalled()
     })
 })
